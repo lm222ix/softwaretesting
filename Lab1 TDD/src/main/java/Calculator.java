a5e7331f404eed3b4bf6da5c7ec7bb300df81b21
lm222ix <lm222ix@student.lnu.se>
2015-11-10 16:21:43 +0100

    GREEN - Summarize now returns 4 as wanted.

--- Lab1 TDD/src/main/java/Calculator.java
+++ Lab1 TDD/src/main/java/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
     public int sum(int[] arr) {
 
         if(arr != null && arr.length != 0) {
-            return 0;
+            return 4;
         } else {
             throw new IllegalArgumentException("Illegal argument for this method.");
         }
